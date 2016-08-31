d0c9f33f75eac5b2725be7eafa9943f9c5f9aefd
Thomas <hello@thomasflorentin.net>
2016-08-31 09:09:47 +0200

    cleaning

--- wp-content/themes/moisphoto/index.php
+++ wp-content/themes/moisphoto/index.php
@@ -31,11 +31,6 @@ get_header(); ?>
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', get_post_format() );
 
 			endwhile;
@@ -52,5 +47,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
