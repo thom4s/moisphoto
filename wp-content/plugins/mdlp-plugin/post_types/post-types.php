ec800a1771b797d5ebea5e0b11419aa9cce84e2f
Thomas <hello@thomasflorentin.net>
2016-08-31 09:09:33 +0200

    Add cursiote poste type

--- wp-content/plugins/mdlp-plugin/post_types/post-types.php
+++ wp-content/plugins/mdlp-plugin/post_types/post-types.php
@@ -1,6 +1,6 @@
 <?php
 
-// Register Custom Post Type
+// Register Place Post Type
 function place_post_type() {
 
   $labels = array(
@@ -43,6 +43,50 @@ function place_post_type() {
 // Hook into the 'init' action
 add_action( 'init', 'place_post_type', 0 );
 
+
+
+// Register Curiosité Post Type
+function curiosite_post_type() {
+
+  $labels = array(
+    'name'                => _x( 'Curiosités', 'Post Type General Name', 'mdlp_db' ),
+    'singular_name'       => _x( 'Curiosité', 'Post Type Singular Name', 'mdlp_db' ),
+    'menu_name'           => __( 'Curiosités', 'mdlp_db' ),
+    'parent_item_colon'   => __( 'Curiosité parente : ', 'mdlp_db' ),
+    'all_items'           => __( 'Toutes les Curiosités', 'mdlp_db' ),
+    'view_item'           => __( 'Voir Curiosité', 'mdlp_db' ),
+    'add_new_item'        => __( 'Ajouter nouvelle curiosité', 'mdlp_db' ),
+    'add_new'             => __( 'Ajouter une curiosité', 'mdlp_db' ),
+    'edit_item'           => __( 'Editer', 'mdlp_db' ),
+    'update_item'         => __( 'Mettre à jour', 'mdlp_db' ),
+    'search_items'        => __( 'Chercher', 'mdlp_db' ),
+    'not_found'           => __( 'Not found', 'mdlp_db' ),
+    'not_found_in_trash'  => __( 'Not found in Trash', 'mdlp_db' ),
+  );
+  $args = array(
+    'label'               => __( 'curiosite', 'mdlp_db' ),
+    'description'         => __( 'Lieux des Curiosités', 'mdlp_db' ),
+    'labels'              => $labels,
+    'supports'            => array( 'title', 'thumbnail', 'revisions', ),
+    'hierarchical'        => true,
+    'public'              => true,
+    'show_ui'             => true,
+    'show_in_menu'        => true,
+    'show_in_nav_menus'   => true,
+    'show_in_admin_bar'   => true,
+    'menu_position'       => 5,
+    'can_export'          => true,
+    'has_archive'         => true,
+    'exclude_from_search' => false,
+    'publicly_queryable'  => true,
+    'capability_type'     => 'page',
+  );
+  register_post_type( 'curiosite', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'curiosite_post_type', 0 );
 
 
 
