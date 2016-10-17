125042d87aa9b1886beb185e4d82cbb6bcbe4c9c
Thomas <hello@thomasflorentin.net>
2016-10-17 11:17:23 +0200

    Add templates for post types

---
+++ wp-content/themes/moisphoto/single-weekend.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying all single weekend post.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package moisphoto
+ */
+
+get_header(); ?>
+ 
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+
+			get_template_part( 'template-parts/content', 'weekend' );
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+
+get_footer();
