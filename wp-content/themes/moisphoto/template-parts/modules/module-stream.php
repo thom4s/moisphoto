8b538dadb808c0290b3b13b09c86ded88be0439c
Thomas <hello@thomasflorentin.net>
2016-11-02 13:22:14 +0100

    Update plugins

--- wp-content/themes/moisphoto/template-parts/modules/module-stream.php
+++ wp-content/themes/moisphoto/template-parts/modules/module-stream.php
@@ -2,23 +2,26 @@
 
 <section class="module-stream">
 
+  <?php echo  do_shortcode('[ff id="1"]'); ?>
+
+
   <div class="stream__item">
     <p class="p--strong">date</p>
     <p class="has-bordertop--little">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis vehicula ante, vitae laoreet nibh. I</p>
-    <div class="pict--rounded--little"> i </div>
+    <div class="arrow--little--black"> i </div>
   </div>
   
   <div class="stream__item">
     <p class="p--strong">date</p>
     <p class="has-bordertop--little">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis vehicula ante, vitae laoreet nibh. I</p>
-    <div class="pict--rounded--little"> t </div>
+    <div class="arrow--little--black"> t </div>
   </div>
 
 
   <div class="stream__item">
     <p class="p--strong">date</p>
     <p class="has-bordertop--little">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis vehicula ante, vitae laoreet nibh. I</p>
-    <div class="pict--rounded--little"> f </div>
+    <div class="arrow--little--black"> t </div>
   </div>
 
 
@@ -26,7 +29,7 @@
     <p class="p--strong">date</p>
     <img src="">
     <p class="has-bordertop--little">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis vehicula ante, vitae laoreet nibh. I</p>
-    <div class="pict--rounded--little"> t </div>
+    <div class="arrow--little--black"> t </div>
   </div>
 
   <?php // echo do_shortcode( '[ff id="1"]' ); ?>
