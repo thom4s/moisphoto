b71b36b33b83b19912f5134a4db643a620863d19
Thomas <hello@thomasflorentin.net>
2016-11-08 14:52:17 +0100

    Polish

--- wp-content/themes/moisphoto/template-parts/content-presse.php
+++ wp-content/themes/moisphoto/template-parts/content-presse.php
@@ -36,73 +36,76 @@
 
     <div class="page__content">
       <?php the_content(); ?>
-    </div><!-- .entry-content -->
 
-    <div class="s-20col m-7col searchbar--press">
-      <?php get_template_part('template-parts/parts/part-presseform'); ?>
-    </div>
-    
+      <div class="s-20col m-9col searchbar--press">
+        <h3 class="h5 has-bordertop--little">Téléchargez les éléments presse par expostion</h3>
+        <?php get_template_part('template-parts/parts/part-presseform'); ?>
+      </div>
+          
 
-    <div id="press__list" class="clearfix m-16col press__list">
+      <div id="press__list" class="clearfix m-16col press__list">
 
-      <?php 
+            <?php 
 
-        $args = array(
-          'posts_per_page'  => -1,
-          'post_type' => array('event'),
-        );
-  
-        $events = new WP_Query( $args );
+              $args = array(
+                'posts_per_page'  => -1,
+                'post_type' => array('event'),
+              );
         
-        if ( $events->have_posts() ) : 
-          while ( $events->have_posts() ) : $events->the_post(); ?>
-          
-            <?php
-                $auteurs = get_field('auteurs'); 
-                $lieu = get_field('lieu');
-                $id = $post->ID;
-            ?>
+              $events = new WP_Query( $args );
+              
+              if ( $events->have_posts() ) : 
+                while ( $events->have_posts() ) : $events->the_post(); ?>
+                
+                  <?php
+                      $auteurs = get_field('auteurs'); 
+                      $lieu = get_field('lieu');
+                      $id = $post->ID;
+                  ?>
 
-            <div class="press__list__item">
-              <p><span class="p--big"><?php moisphoto_get_artists_list($auteurs); ?></span> 
-                <span class="p--strong">- <?php the_title(); ?></span>
-                <span class=""> | <?php echo get_the_title( $lieu ); ?></span>
-              </p>
-              <?php if( $dp_pdf || $visuels_zip ) : ?>
-                <p class="press__btn">
-                  <span class="p--strong">Téléchargez :  </span>
-                  <?php if( $dp_pdf ) : ?>
-                    <a href="<?php the_permalink(); ?>?pdf=" classe="" target="_blank">le DP (.pdf)</a> -  
-                  <?php endif; ?> 
-                  <?php if( $visuels_zip ) : ?>
-                    <a href="?zip=<?php echo $id; ?>" classe="">Les visuels (zip)</a>
-                  <?php endif; ?>
-                </p>
-              <?php endif; ?> 
+                  <div class="press__list__item">
+                    <p><span class="p--big"><?php moisphoto_get_artists_list($auteurs); ?></span> 
+                      <span class="p--strong">- <?php the_title(); ?></span>
+                      <span class=""> | <?php echo get_the_title( $lieu ); ?></span>
+                    </p>
+                    <?php if( $dp_pdf || $visuels_zip ) : ?>
+                      <p class="press__btn">
+                        <span class="p--strong">Téléchargez :  </span>
+                        <?php if( $dp_pdf ) : ?>
+                          <a href="<?php the_permalink(); ?>?pdf=" classe="" target="_blank">le DP (.pdf)</a> -  
+                        <?php endif; ?> 
+                        <?php if( $visuels_zip ) : ?>
+                          <a href="?zip=<?php echo $id; ?>" classe="">Les visuels (zip)</a>
+                        <?php endif; ?>
+                      </p>
+                    <?php endif; ?> 
+                  </div>
+                
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+
+              <?php else : ?>
+                <p class="no-result"><?php _e( 'Désolé, il n\'y a aucun résultat pour votre recherche.' ); ?></p>
+
+              <?php endif; ?>
+
+          </div>
+
+
+        <?php else: ?>
+
+          <div class="press__password row">
+            <div class="m-10col is-centered  ">
+              <?php echo get_the_password_form(); ?> 
             </div>
-          
-          <?php endwhile; ?>
-          <?php wp_reset_postdata(); ?>
-
-        <?php else : ?>
-          <p class="no-result"><?php _e( 'Désolé, il n\'y a aucun résultat pour votre recherche.' ); ?></p>
-
+          </div>
+         
         <?php endif; ?>
 
-    </div>
+    </div><!-- .entry-content -->
 
 
-  <?php else: ?>
-
-    <div class="press__password row">
-      <div class="m-10col is-centered  ">
-        <?php echo get_the_password_form(); ?> 
-      </div>
-    </div>
-   
-  <?php endif; ?>
-
-
+  
   </div>
 </article><!-- #post-## -->
 
