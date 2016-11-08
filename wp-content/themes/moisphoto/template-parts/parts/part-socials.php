0586739c894918e9f687c902276722c459c415b7
Thomas <hello@thomasflorentin.net>
2016-11-08 11:49:45 +0100

    Remove twiiter & Insta. Add contact

--- wp-content/themes/moisphoto/template-parts/parts/part-socials.php
+++ wp-content/themes/moisphoto/template-parts/parts/part-socials.php
@@ -14,29 +14,38 @@
             </div>        
           </div>
 
-          <div class="pict--social square s-2col m-1col s-last">
+<!--           <div class="pict--social square s-2col m-1col s-last">
             <div class="square__content">
               <div class="table">
                 <a href="<?php echo TWITTER_LINK; ?>" target="_blank" class="pict__content icon-twitter table-cell"> </a>
               </div>
             </div>
-          </div>
+          </div> -->
           
-          <div class="pict--social square s-2col m-1col s-last">
+
+          <div class="pict--contact square s-2col m-1col s-last">
             <div class="square__content">
               <div class="table">
-                <a href="<?php echo FACEBOOK_LINK; ?>" target="_blank" class="pict__content icon-facebook table-cell"> </a>
+                <a href="/equipe-et-contacts" class="pict__content icon table-cell">Con-<br>tact</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="pict--fb square s-2col m-1col s-last">
+            <div class="square__content">
+              <div class="table">
+                <a href="<?php echo FACEBOOK_LINK; ?>" target="_blank" class="pict__content icon-facebook table-cell"></a>
               </div>
             </div>
           </div>
   
-          <div class="pict--social square s-2col m-1col s-last">
+<!--           <div class="pict--social square s-2col m-1col s-last">
             <div class="square__content">
               <div class="table">
                 <a href="<?php echo INSTAGRAM_LINK; ?>" target="_blank" class="pict__content icon-instagram table-cell"></a>
               </div>
             </div>
-          </div>
+          </div> -->
 
           <div id="searchbar" class="searchbar s-24col m-6col">
             <?php get_search_form(); ?>
