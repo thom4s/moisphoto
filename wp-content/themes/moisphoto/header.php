99438f275931e24ff092f0185eb8c97cbdc5fb41
Thomas <hello@thomasflorentin.net>
2016-11-24 14:29:24 +0100

    Add favico

--- wp-content/themes/moisphoto/header.php
+++ wp-content/themes/moisphoto/header.php
@@ -12,6 +12,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.png" />
 
 <?php wp_head(); ?>
 </head>
