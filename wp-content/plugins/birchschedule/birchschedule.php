83d282fefd94a442c091560ebc9bdace6b11c17b
Thomas <hello@thomasflorentin.net>
2017-03-23 16:06:28 +0100

    Add rdv plugin for tests

---
+++ wp-content/plugins/birchschedule/birchschedule.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+  Plugin Name: BirchPress Scheduler
+  Plugin URI: http://www.birchpress.com
+  Description: An appointment booking and online scheduling plugin that allows service businesses to take online bookings.
+  Version: 1.10.10
+  Author: BirchPress
+  Author URI: http://www.birchpress.com
+  License: GPLv2
+ */
+
+if ( defined( 'ABSPATH' ) && ! function_exists( 'birchschedule_main' ) ) {
+
+	function birchschedule_main() {
+
+		require_once 'lib/vendor/autoload.php';
+		require_once 'loader.php';
+
+		birchschedule_load( array(
+				'plugin_file_path' => __FILE__,
+				'product_version' => '1.10.10',
+				'product_name' => 'BirchPress Scheduler',
+				'product_code' => 'birchschedule',
+				'global_name' => 'birchschedule'
+			) );
+	}
+
+	birchschedule_main();
+}
